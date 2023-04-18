17f60a72cac8d32ed71ac64a9ea092227924e1c1
rafa-ell <rafaelgabert@hotmail.com>
2023-04-18 19:12:53 -0300

    .

--- acoes/salvar_cliente.php
+++ acoes/salvar_cliente.php
@@ -4,16 +4,17 @@ session_start();
 require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . '/acoes/verifica_sessao.php');
 require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/classes/cliente.class.php");
 require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/controllers/cliente.controller.php");
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/DAO/LoginDAO.php");
+require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "/classes/login.class.php");
 
 $cliente = new Cliente();
 
-if (isset($_POST) && isset($_POST['id'])) {
+if (isset($_POST) && isset($_POST['id']) && !empty($_POST['id'])) {
     $id         = addslashes(filter_input(INPUT_POST, 'id'));
     $nome       = addslashes(filter_input(INPUT_POST, 'nome'));
     $cpfcnpj    = addslashes(filter_input(INPUT_POST, 'cpfcnpj'));
     $telefone   = addslashes(filter_input(INPUT_POST, 'telefone'));
     $email   = addslashes(filter_input(INPUT_POST, 'email'));
-    $senha   = addslashes(filter_input(INPUT_POST, 'senha'));
     var_dump($cpfcnpj);
 
     if (empty($nome) || empty($cpfcnpj)) {
@@ -24,50 +25,59 @@ if (isset($_POST) && isset($_POST['id'])) {
     }
 
 
-    $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
-    $cpfcnpj = isset($_POST['cpfcnpj']) ? $_POST['cpfcnpj'] : null;
-    $telefone = isset($_POST['telefone']) ? $_POST['telefone'] : null;
-    $email = isset($_POST['email']) ? $_POST['email'] : null;
-    $senha = isset($_POST['senha']) ? $_POST['senha'] : null;
+    $cliente->setId($id);
+    $cliente->setNome($nome);
+    $cliente->setCpfCnpj($cpfcnpj);
+    $cliente->setTelefone($telefone);
+    $cliente->setEmail($email);
+    
 
-    if ($nome && $cpfcnpj) {
+    $controller = new ClienteController();
+    $resultado = $controller->atualizarCliente($cliente);
 
-        $cliente->setNome($nome);
-        $cliente->setCpfCnpj($cpfcnpj);
-        $cliente->setTelefone($telefone);
-        $cliente->setEmail($email);
-        $cliente->setSenha($senha);
-
-        $dao = new ClienteController();
-        $resultado = $dao->criarCliente($cliente);
-        if ($resultado) {
-            $_SESSION['mensagem'] = "Criado com sucesso";
-            $_SESSION['sucesso'] = true;
-        } else {
-            $_SESSION['mensagem'] = "Erro ao criar";
-            $_SESSION['sucesso'] = false;
-        }
+    if ($resultado) {
+        $_SESSION['mensagem'] = "Atualizado com sucesso";
+        $_SESSION['sucesso'] = true;
     } else {
-        $_SESSION['mensagem'] = "Obrigatório informar Nome e CPF/CNPJ";
+        $_SESSION['mensagem'] = "Erro ao atualizar";
         $_SESSION['sucesso'] = false;
     }
-    header('Location:../public/cad_cliente.php');
-    // $cliente->setId($id);
-    // $cliente->setNome($nome);
-    // $cliente->setCpfCnpj($cpfcnpj);
-    // $cliente->setTelefone($telefone);
-    // $cliente->setEmail($email);
-    // $cliente->setSenha($senha);
+} else {
+    $id         = addslashes(filter_input(INPUT_POST, 'id'));
+    $nome       = addslashes(filter_input(INPUT_POST, 'nome'));
+    $cpfcnpj    = addslashes(filter_input(INPUT_POST, 'cpfcnpj'));
+    $telefone   = addslashes(filter_input(INPUT_POST, 'telefone'));
+    $email   = addslashes(filter_input(INPUT_POST, 'email'));
+    $senha   = addslashes(filter_input(INPUT_POST, 'senha'));
 
-    // $controller = new ClienteController();
-    // $resultado = $controller->atualizarCliente($cliente);
+    if (empty($nome) || empty($cpfcnpj)) {
+        $_SESSION['mensagem'] = "Obrigatório informar Nome e CPF/CNPJ";
+        $_SESSION['sucesso'] = false;
+        // header('Location:../public/cad_cliente.php?key=' . $id);
+        // echo 'rwerew';
+        die();
+    }
 
-    // if ($resultado) {
-    //     $_SESSION['mensagem'] = "Atualizado com sucesso";
-    //     $_SESSION['sucesso'] = true;
-    // } else {
-    //     $_SESSION['mensagem'] = "Erro ao atualizar";
-    //     $_SESSION['sucesso'] = false;
-    // }
-    // header('Location:../public/cad_cliente.php');
-} 
+    $cliente->setNome($nome);
+    $cliente->setCpfCnpj($cpfcnpj);
+    $cliente->setTelefone($telefone);
+    $cliente->setEmail($email);
+
+    $dao = new ClienteController();
+    $resultado = $dao->criarCliente($cliente);
+    if ($resultado) {
+        $login = new Login();
+        $login->setEmail($email);
+        $login->setSenha($senha);
+        $login->setAtivo(true);
+        $login->setTipo("user");
+        $dao = new LoginDAO();
+        $dao->inserirLogin($login);
+        $_SESSION['mensagem'] = "Criado com sucesso";
+        $_SESSION['sucesso'] = true;
+    } else {
+        $_SESSION['mensagem'] = "Erro ao criar";
+        $_SESSION['sucesso'] = false;
+    }
+}
+header('Location:../public/cad_cliente.php');
--- public/logoff.php
+++ public/logoff.php
@@ -3,7 +3,7 @@
 session_start();
 
 unset($_SESSION["usuario_email"]);
-
+unset($_SESSION['usuario_id']);
 header("Location:index.php");
 
 die();
